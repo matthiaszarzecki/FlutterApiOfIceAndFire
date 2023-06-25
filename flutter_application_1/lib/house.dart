1c130339f75919ccab3e59a24924aa4e36554265
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2023-06-26 01:17:38 +0200

    Add elements to house view

--- flutter_application_1/lib/house.dart
+++ flutter_application_1/lib/house.dart
@@ -3,6 +3,7 @@ class House {
   final String region;
   final String coatOfArms;
   final String words;
+  
   final List<dynamic> titles; // TODO: Figure out how to do string arrays
   final List<dynamic> seats;
 
--- flutter_application_1/lib/house_view.dart
+++ flutter_application_1/lib/house_view.dart
@@ -8,16 +8,59 @@ class SingleHouseDisplay extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(
-          house.name,
-          style: Theme.of(context).textTheme.headlineSmall,
-        ),
-        Text(house.region),
-        Text("🛡️ ${house.coatOfArms.toString()}"),
-        Text("🪶 ${house.words.toString()}"),
-      ],
-    );
+    List<Widget> elements = [];
+
+    elements.add(Text(
+      house.name,
+      style: Theme.of(context).textTheme.headlineSmall,
+    ));
+    elements.add(const SizedBox(height: 10));
+
+    if (house.region.isNotEmpty) {
+      elements.add(Text("of ${house.region.toString()}"));
+      elements.add(const SizedBox(height: 10));
+    }
+
+    if (house.coatOfArms.isNotEmpty) {
+      elements.add(Text("🛡️ ${house.coatOfArms.toString()}"));
+      elements.add(const SizedBox(height: 10));
+    }
+
+    if (house.words.isNotEmpty) {
+      elements.add(Text("🪶 ${house.words.toString()}"));
+      elements.add(const SizedBox(height: 10));
+    }
+
+    if (house.currentLord.isNotEmpty) {
+      elements.add(Text("Current Lord: ${house.currentLord.toString()}"));
+      elements.add(const SizedBox(height: 10));
+    }
+
+    if (house.heir.isNotEmpty) {
+      elements.add(Text("Heir: ${house.heir.toString()}"));
+      elements.add(const SizedBox(height: 10));
+    }
+
+    if (house.overlord.isNotEmpty) {
+      elements.add(Text("Overlord: ${house.overlord.toString()}"));
+      elements.add(const SizedBox(height: 10));
+    }
+
+    if (house.founded.isNotEmpty) {
+      elements.add(Text("Founded: ${house.founded.toString()}"));
+      elements.add(const SizedBox(height: 10));
+    }
+
+    if (house.founder.isNotEmpty) {
+      elements.add(Text("Founder: ${house.founder.toString()}"));
+      elements.add(const SizedBox(height: 10));
+    }
+
+    if (house.diedOut.isNotEmpty) {
+      elements.add(Text("💀 Died out: ${house.diedOut.toString()}"));
+      elements.add(const SizedBox(height: 10));
+    }
+
+    return Column(children: elements);
   }
 }
