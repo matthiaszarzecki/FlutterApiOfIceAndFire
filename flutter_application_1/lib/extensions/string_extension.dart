b373fc7ce2bc3293113433d3cc83989742c772e0
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2023-06-28 12:47:41 +0200

    Add comment

--- flutter_application_1/lib/extensions/string_extension.dart
+++ flutter_application_1/lib/extensions/string_extension.dart
@@ -1,3 +1,5 @@
+/// Returns the character at the specified index of a string.
+/// Returns an empty string if the index is out of bounds.
 extension GetSafeCharacter on String {
   String safeIndex(int index) => index < 0 || index >= length ? "" : this[index];
 }
