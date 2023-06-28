992c0c576677f31de05de0157fbb46c4e6148194
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2023-06-28 12:03:00 +0200

    Add working unit test for house initial

--- flutter_application_1/test/widget_test.dart
+++ flutter_application_1/test/widget_test.dart
@@ -1,8 +1,20 @@
 import 'package:flutter_application_1/app/got_app.dart';
+import 'package:flutter_application_1/models/house_basic.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('Smoke test', (WidgetTester tester) async {
     await tester.pumpWidget(GOTApp());
+  });
+
+  test('HouseBasic Initial should return correct letter', () {
+    // GIVEN a house basic
+    const mockHouse = HouseBasic.mockHouseBasic;
+
+    // WHEN we access the initial property
+    String result = mockHouse.initial();
+
+    // THEN the first letter of the house name is returned
+    expect(result, "G");
   });
 }
