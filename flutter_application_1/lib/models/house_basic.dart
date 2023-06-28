06406a2366ff7c21671345879ae08269e4ad61a1
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2023-06-28 11:46:42 +0200

    Add mock house, url and culture fields to house basic

--- flutter_application_1/lib/models/house_basic.dart
+++ flutter_application_1/lib/models/house_basic.dart
@@ -1,10 +1,12 @@
 // The house as it is returned from the API.
 class HouseBasic {
+  final String url;
   final String name;
   final String region;
   final String coatOfArms;
   final String words;
-  
+  final String culture;
+
   final List<dynamic> titles;
   final List<dynamic> seats;
 
@@ -24,6 +26,7 @@ class HouseBasic {
   }
 
   const HouseBasic({
+    required this.url,
     required this.name,
     required this.region,
     required this.coatOfArms,
@@ -39,12 +42,15 @@ class HouseBasic {
     required this.ancestralWeapons,
     required this.cadetBranches,
     required this.swornMembers,
+    required this.culture,
   });
 
   factory HouseBasic.fromJson(Map<String, dynamic> json) {
     return HouseBasic(
+      url: json['url'],
       name: json['name'],
       region: json['region'],
+      culture: json['culture'],
       coatOfArms: json['coatOfArms'],
       words: json['words'],
       titles: json['titles'],
@@ -60,4 +66,40 @@ class HouseBasic {
       swornMembers: json['swornMembers'],
     );
   }
+
+  static const HouseBasic mockHouseBasic = HouseBasic(
+    ancestralWeapons: ["Brightroar", "Lamentation", "Orphan-Maker"],
+    cadetBranches: [
+      "https://www.anapioficeandfire.com/api/houses/285",
+      "https://www.anapioficeandfire.com/api/houses/286",
+      "https://www.anapioficeandfire.com/api/houses/287"
+    ],
+    coatOfArms: "A Gold Dove on a Green Field",
+    currentLord: "https://www.anapioficeandfire.com/api/characters/298",
+    diedOut: "During the Andals invasion",
+    founder: "https://www.anapioficeandfire.com/api/characters/298",
+    founded: "120 AC",
+    heir: "https://www.anapioficeandfire.com/api/characters/298",
+    name: "House Goodbrother of Crow Spike Keep",
+    overlord: "https://www.anapioficeandfire.com/api/houses/285",
+    region: "Dorne",
+    seats: [
+      "Godsgrace",
+      "Gates of the Moon (winter)",
+    ],
+    swornMembers: [
+      "https://www.anapioficeandfire.com/api/characters/298",
+      "https://www.anapioficeandfire.com/api/characters/299",
+      "https://www.anapioficeandfire.com/api/characters/300",
+    ],
+    titles: [
+      "King of Mountain and Vale (formerly)",
+      "Lord of the Eyrie",
+      "Defender of the Vale",
+      "Warden of the East",
+    ],
+    url: "https://www.anapioficeandfire.com/api/house/7",
+    words: "No Foe May Pass",
+    culture: "Northmen",
+  );
 }
