211917539f5648a5ed1b6ef1140eb828d032ea9e
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2023-06-29 11:13:38 +0200

    Improve house view

--- flutter_application_1/lib/views/house_cell.dart
+++ flutter_application_1/lib/views/house_cell.dart
@@ -12,50 +12,52 @@ class HouseCell extends StatelessWidget {
     double width = MediaQuery.of(context).size.width;
     double height = 40 + 10 * 2;
 
-    return SizedBox(
-      width: width,
-      height: height,
-      child: Column(
-        children: [
-          const SizedBox(height: 10),
-          InkWell(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => Scaffold(
-                    appBar: AppBar(
-                      title: Text(house.name),
+    return Column(
+      children: [
+        const SizedBox(height: 6),
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.red,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: SizedBox(
+            width: width,
+            height: 60,
+            child: InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Scaffold(
+                      appBar: AppBar(
+                        title: Text(house.name),
+                      ),
+                      body: SingleHouseLoader(houseBasic: house),
                     ),
-                    body: SingleHouseLoader(houseBasic: house),
                   ),
-                ),
-              );
-            },
-            child: Stack(
-              children: [
-                Ink(height: 40, width: width, color: Colors.grey),
-                Column(
-                  children: [
-                    const SizedBox(height: 7),
-                    Row(
-                      children: [
-                        CircleAvatar(
-                          radius: 12,
-                          child: Text(house.initial()),
-                        ),
-                        const SizedBox(width: 10),
-                        Text(house.name),
-                      ],
-                    )
-                  ],
-                ),
-              ],
+                );
+              },
+              child: Column(
+                children: [
+                  const SizedBox(height: 17),
+                  Row(
+                    children: [
+                      const SizedBox(width: 10),
+                      CircleAvatar(
+                        radius: 12,
+                        child: Text(house.initial()),
+                      ),
+                      const SizedBox(width: 10),
+                      Text(house.name),
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
-          const SizedBox(height: 10)
-        ],
-      ),
+        ),
+        const SizedBox(height: 6),
+      ],
     );
   }
 }
