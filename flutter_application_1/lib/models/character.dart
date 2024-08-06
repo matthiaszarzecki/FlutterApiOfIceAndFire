0b43f2a9c8b20302df37355a05e47aff45d3505e
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2024-08-06 09:54:39 +0200

    Extend character

--- flutter_application_1/lib/models/character.dart
+++ flutter_application_1/lib/models/character.dart
@@ -1,16 +1,64 @@
 class Character {
   final String url;
   final String name;
+  final String? culture;
+  final String? born;
+  final String? died;
 
   const Character({
     required this.url,
     required this.name,
+    required this.culture,
+    required this.born,
+    required this.died
   });
 
   factory Character.fromJson(Map<String, dynamic> json) {
     return Character(
       url: json['url'],
       name: json['name'],
+      culture: json['culture'],
+      born: json['born'],
+      died: json['died']
     );
   }
+
+  bool hasInformation() {
+    if (culture != null && culture != "") {
+      return true;
+    }
+    if (born != null && born != "") {
+      return true;
+    }
+    if (died != null && died != "") {
+      return true;
+    }
+
+    return false;
+  }
+
+  /*bool hasInformation:  {
+    // We are leaving out the "url" field as that URL
+    // leads back to this character, and not anywhere else.
+    culture.exists ||
+      born.exists ||
+      died.exists ||
+      titles.hasNonEmptyEntries ||
+      aliases.hasNonEmptyEntries ||
+      tvShowAppearances.hasNonEmptyEntries ||
+      portrayedBy.hasNonEmptyEntries
+  }*/
 }
+
+/*
+let url: String
+  let name: String
+  let displayName: String
+  let culture: String
+  let born: String
+  let died: String
+  let titles: [String]
+  let aliases: [String]
+  let tvShowAppearances: [String]
+  let portrayedBy: [String]
+*/
