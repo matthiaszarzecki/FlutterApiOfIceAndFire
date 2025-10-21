bb136e1b0f4909e836cf6f4d9b9bc59800c1d2e3
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2025-10-21 14:59:36 +0200

    Update api logic

--- flutter_application_1/lib/helper/api.dart
+++ flutter_application_1/lib/helper/api.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:flutter_application_1/extensions/int_extension.dart';
 import 'package:flutter_application_1/helper/uri_handler.dart';
 import 'package:flutter_application_1/models/all_houses_response.dart';
 import 'package:flutter_application_1/models/character.dart';
@@ -8,37 +9,44 @@ import 'package:http/http.dart' as http;
 class API {
   static final instance = API();
 
+  /// Fetches a list of HouseBasics
   Future<List<HouseBasic>?> getHouses(int page) async {
     final dynamic response = await http.get(URIHandler.getHousesUri(page));
 
-    if (response.statusCode >= 200 && response.statusCode <= 299) {
-      List<HouseBasic> newHouses =
-          AllHousesResponse.fromJson(jsonDecode(response.body)).houses;
+    final int responseCode = response.statusCode;
+    if (responseCode.isSuccessful()) {
+      List<HouseBasic> newHouses = AllHousesResponse.fromJson(
+        jsonDecode(response.body),
+      ).houses;
 
       if (newHouses.isNotEmpty) {
         return newHouses;
-      } 
+      }
       return null;
     }
     return null;
   }
 
+  /// Fetches the specified Character
   Future<Character?> getCharacter(String characterURI) async {
     Uri uri = Uri.parse(characterURI);
     final response = await http.get(uri);
 
-    if (response.statusCode >= 200 && response.statusCode <= 299) {
+    final int responseCode = response.statusCode;
+    if (responseCode.isSuccessful()) {
       Character character = Character.fromJson(jsonDecode(response.body));
       return character;
-    } 
+    }
     return null;
   }
 
+  /// Fetches the specified HouseBasic
   Future<HouseBasic?> getSingleHouse(String houseURI) async {
     Uri uri = Uri.parse(houseURI);
     final response = await http.get(uri);
 
-    if (response.statusCode >= 200 && response.statusCode <= 299) {
+    final int responseCode = response.statusCode;
+    if (responseCode.isSuccessful()) {
       HouseBasic houseBasic = HouseBasic.fromJson(jsonDecode(response.body));
       return houseBasic;
     }
